36cda7019f69c3423f2f4d4c5727a5e8ee3d7774
Willian Trindade Leite <williantleite@gmail.com>
2022-03-30 19:02:05 +0200

    Improved quarter to month script to fix the annoying case of anxious_index set

--- Dev/Quarter to month.py
+++ Dev/Quarter to month.py
@@ -20,3 +20,17 @@ anxious_index_df = pd.read_excel(r"C:\Users\willi\Documents\Python\Thesis\Data\O
 anxious_index_df.columns = ["year", "quarter", "anxious_index", "recess"]
 
 anxious_index_df = anxious_index_df.iloc[3:,0:3]
+month_dict = {1:"01", 2:"04", 3:"07", 4:"10"}
+anxious_index_df["month"] = anxious_index_df["quarter"].apply(lambda x:month_dict[x])
+anxious_index_df["year"] = anxious_index_df["year"].astype(str)
+anxious_index_df["DATE"] = anxious_index_df[["year", "month"]].agg("-".join, axis=1)
+
+def date_addition(df):
+    df["DATE"] = pd.to_datetime(df["DATE"]).dt.to_period("M")
+    df = df.set_index("DATE").resample("M").pad(limit=3)
+    df["month"] = df.index.month
+    df.insert(1, "month", df.pop("month"))
+    return(df)
+
+anxious_index_df = date_addition(anxious_index_df)
+anxious_index_df.pop("quarter")
