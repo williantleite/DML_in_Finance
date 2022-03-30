d9e0877f77cccf2bd6004f800355f8d9fab6bdf6
Willian Trindade Leite <williantleite@gmail.com>
2022-03-30 18:02:39 +0200

    Added a draft to a quarter-to-month transformer. Further changes to the data collection script limiting the time frame further to make it more manageable when merging with other datasets.

--- Dev/Data collection.py
+++ Dev/Data collection.py
@@ -15,7 +15,7 @@ passive_list = pd.read_csv(r"C:\Users\willi\Documents\Python\Thesis\Data\List 19
                            names = ["ticker", "name", "inception_date",
                                     "fund_asset_class_focus", "parent_comp_name",
                                     "fund_industry_focus", "tot_asset_mil"], 
-                           header=0)
+                           header = 0)
 
 passive_list["ticker"] = passive_list["ticker"].str.replace(r'\sUS Equity', '')
 
@@ -23,7 +23,7 @@ active_list = pd.read_csv(r"C:\Users\willi\Documents\Python\Thesis\Data\List 497
                           names = ["ticker", "name", "inception_date",
                                    "fund_asset_class_focus", "parent_comp_name",
                                    "fund_industry_focus", "tot_asset_mil"], 
-                          header=0)
+                          header = 0)
 
 active_list["ticker"] = active_list["ticker"].str.replace(r'\sUS Equity', '')
 
@@ -31,22 +31,22 @@ passive_tickers = passive_list["ticker"].tolist()
 active_tickers = active_list["ticker"].tolist()
 
 passive_df = yf.download(passive_tickers,
-                         start = "1985-01-01",
-                         end = "2022-01-31",
+                         start = "1986-01-31",
+                         end = "2022-01-01",
                          interval = "1mo",
                          progress = True)
 
 passive_df = passive_df["Close"]
 
-passive_df = passive_df.dropna(how="all", axis=1)
-passive_df = passive_df.dropna(how="all", axis=0)
+passive_df = passive_df.dropna(how = "all", axis = 1)
+passive_df = passive_df.dropna(how = "all", axis = 0)
 passive_df["year"], passive_df["month"] = passive_df.index.year, passive_df.index.month
 passive_df.insert(0, "year", passive_df.pop("year"))
 passive_df.insert(1, "month", passive_df.pop("month"))
 
 active_df = yf.download(active_tickers, 
-                        start = "1985-01-01",
-                        end = "2022-01-31",
+                        start = "1986-01-31",
+                        end = "2022-01-01",
                         interval = "1mo",
                         progress = True)
 
