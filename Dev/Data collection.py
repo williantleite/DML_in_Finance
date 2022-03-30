839548ce9c6324056b93b2407f5ad2e9bccfa3ef
Willian Trindade Leite <williantleite@gmail.com>
2022-03-30 16:19:31 +0200

    Updating data collection script to fix some problems caused by yfinance

--- Dev/Data collection.py
+++ Dev/Data collection.py
@@ -11,7 +11,7 @@ import pandas as pd
 import yfinance as yf
 from yahoofinancials import YahooFinancials
 
-passive_list = pd.read_csv(r"C:\Users\willi\Desktop\Passive 1948 csv.csv", 
+passive_list = pd.read_csv(r"C:\Users\willi\Documents\Python\Thesis\Data\List 1948 Passive Funds.csv", 
                            names = ["ticker", "name", "inception_date",
                                     "fund_asset_class_focus", "parent_comp_name",
                                     "fund_industry_focus", "tot_asset_mil"], 
@@ -19,7 +19,7 @@ passive_list = pd.read_csv(r"C:\Users\willi\Desktop\Passive 1948 csv.csv",
 
 passive_list["ticker"] = passive_list["ticker"].str.replace(r'\sUS Equity', '')
 
-active_list = pd.read_csv(r"C:\Users\willi\Desktop\Active 4976 csv.csv", 
+active_list = pd.read_csv(r"C:\Users\willi\Documents\Python\Thesis\Data\List 4976 Active Funds.csv", 
                           names = ["ticker", "name", "inception_date",
                                    "fund_asset_class_focus", "parent_comp_name",
                                    "fund_industry_focus", "tot_asset_mil"], 
@@ -31,18 +31,33 @@ passive_tickers = passive_list["ticker"].tolist()
 active_tickers = active_list["ticker"].tolist()
 
 passive_df = yf.download(passive_tickers,
-                         period = "max",
+                         start = "1985-01-01",
+                         end = "2022-01-31",
                          interval = "1mo",
                          progress = True)
 
 passive_df = passive_df["Close"]
 
+passive_df = passive_df.dropna(how="all", axis=1)
+passive_df = passive_df.dropna(how="all", axis=0)
+passive_df["year"], passive_df["month"] = passive_df.index.year, passive_df.index.month
+passive_df.insert(0, "year", passive_df.pop("year"))
+passive_df.insert(1, "month", passive_df.pop("month"))
+
 active_df = yf.download(active_tickers, 
-                        period = "max",
+                        start = "1985-01-01",
+                        end = "2022-01-31",
                         interval = "1mo",
                         progress = True)
 
 active_df = active_df["Close"]
 
+active_df = active_df.dropna(how="all", axis=1)
+active_df = active_df.dropna(how="all", axis=0)
+active_df["year"], active_df["month"] = active_df.index.year, active_df.index.month
+active_df.insert(0, "year", active_df.pop("year"))
+active_df.insert(1, "month", active_df.pop("month"))
+active_df = active_df.iloc[9:,] #We do this so that the active funds dataset has the same shape as the passive funds dataset.
+    
 passive_df.to_csv('passive_df.csv', index=False)
 active_df.to_csv('active_df.csv', index=False)
